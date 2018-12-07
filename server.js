16556564fbecd5435da8a9ddaf15e1c94927f0e0
Nitish <nitishmnka@gmail.com>
2018-12-07 14:40:49 +0530

    initial comit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express =require('express');
+const hbs=require('hbs');
+const fs =require('fs');
+
+
+var app =express();
+hbs.registerPartials(__dirname +'/views/partials');
+hbs.registerHelper("getcurrentyear",()=>
+{
+  return new Date().getFullYear();
+});
+hbs.registerHelper("screamit",(text)=>
+{
+  return text.toUpperCase();
+});
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+app.use((req,res,next)=>
+{var now =new Date().toString();
+  var log=`${now}, ${req.method},${req.url}`;
+  fs.appendFile('server.log',log +'\n',(err)=>
+{if(err)
+  {
+  console.log("unable to append");
+}
+});
+  console.log(log);
+next();
+});
+
+app.get('/',(req,res)=>
+{//res.send("<h1>hello</h1>");
+res.render('home.hbs',{
+  pageTitle:"aboutpage",
+
+welcomemsg : "hello welcome to my page"
+});
+
+});
+app.get('/about',(req,res)=>
+{
+  res.render("about.hbs",{
+    pageTitle:"aboutpage",
+
+  });
+});
+app.get('/bad',(req,res)=>
+{
+  res.send({
+    error : "badmessage"
+  })
+});
+app.listen(3000,()=>
+{
+  console.log("server is up on port 3000");
+});
